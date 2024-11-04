f767c714d91514bbc3cfc9f77130bd787e0ced36
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2024-11-04 22:44:43 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     nezha_command = [
         agent_path,
         "-s", "tzz.shiyue.eu.org:5555",
-        "-p", "HAYZTY8sq79nhWpFcA",
+        "-p", "n99x84VFS3HBRi722T",
         "-d"
     ]
     subprocess.Popen(nezha_command)
